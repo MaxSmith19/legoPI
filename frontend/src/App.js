0140200ec585d22a98f89868814c1dff378e039f
Max Smith <blackblob3@gmail.com>
2023-12-08 18:29:40 +0000

    added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Login from './pages/loginCreate'
+import Login from './pages/LoginCreate'
+import Dashboard from './pages/Dashboard'
 import { Navigate } from 'react-router-dom';
+
 import { useEffect, useState } from 'react';
+import Navigation from './components/Navigation';
+
 function App() {
   return (
     <>
-      <div className="bg-blue-500">
+      <div className="bg-gray-200 flex flex-row-2">
         <Router>
-
+          <Navigation />
             <Routes>
               <Route path="/" exact element={<Navigate replace to="/Login"></Navigate>} />
               <Route path="/Login" element={<Login />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
             </Routes>
               
           </Router>
