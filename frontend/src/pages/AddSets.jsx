f6dc24600a4de6410e779c48d12b71244fc8951c
Max Smith <blackblob3@gmail.com>
2024-02-05 22:40:13 +0000

    Wanted set functionality

--- frontend/src/pages/AddSets.jsx
+++ frontend/src/pages/AddSets.jsx
@@ -2,23 +2,24 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useState } from 'react';
+import Switch from 'react-switch';
 
 const AddSets = () => {
     const [set, setSet] = useState([])
     const [setCode, setSetCode] = useState('')
     const [isAutoFilled, setIsAutoFilled] = useState(false)
+    const [wanted, setWanted] = useState(true);
+
     useEffect(() => {
         setIsAutoFilled(false)
         console.log(isAutoFilled)
     },[])
     
     useEffect(() => {
-        console.log(set["Year released"]);
-    }, [set]);
+        console.log(wanted)
+    },[wanted])
 
     const autoFillSet = async () => {
-        console.log(setCode);
-    
         let config = {
             method: 'get',
             maxBodyLength: Infinity,
@@ -37,6 +38,7 @@ const AddSets = () => {
         }
     };
     const confirmSet = async () => {
+        wanted ? set.wanted=true : set.wanted=false
         let config = {
             method: 'post',
             maxBodyLength: Infinity,
@@ -56,33 +58,34 @@ const AddSets = () => {
         }
     }
     return (
-        <div  className="pt-8 px-20 w-full">
+        <div className={wanted ? "text-center bg-blue-300 pt-8 px-20 w-full transition-all delay-75" : "text-center bg-red-300 pt-8 px-20 w-full transition-all delay-75"}>
             <div className="text-center">
-                <h1 className="text-5xl border-b-2 border-gray-300 ">Add a set</h1>
-                <div className="flex flex-wrap justify-center">
+                <h1 className="text-5xl">Add a{wanted ? "n owned" : " wanted"} set</h1>
+                <div className="flex flex-row"><h2 className="mr-3"><strong>Wishlist?</strong></h2><Switch onChange={()=>setWanted(!wanted)} checked={!wanted}/></div>
+                <div className="flex flex-wrap mt-8 flex-row mb-10">
                     <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setCode" type="text" placeholder="Set Code for your lego set" onChange={(e)=>setSetCode(e.target.value)}/>
                     <button className={isAutoFilled? "hidden":"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"} onClick={()=>autoFillSet()}>AutoFill</button>
                 </div>
                 <div>
-                    <div className="flex flex-wrap justify-center">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="setCode"> Name of the set </label>
-                        <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setName" type="text" placeholder="Set Name" value={set.Name} onChange={(e)=>setSet({...set, Name: e.target.value})}/>
+                    <div className="flex flex-wrap flex-row justify-center">
+                        <label className="block w-1/6 mr-1 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" htmlFor="setCode"> Name of the set </label>
+                        <input className="block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" id="setName" type="text" placeholder="Set Name" value={set.Name} onChange={(e)=>setSet({...set, Name: e.target.value})}/>
                     </div>
                     <div className="flex flex-wrap justify-center">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="setCode"> Theme </label>
-                        <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setTheme" type="text" placeholder="Set Theme" value={set["Theme"]} onChange={(e)=>setSet({...set, Theme: e.target.value})}/>
+                        <label className="block w-1/6 mr-1 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" htmlFor="setCode"> Theme </label>
+                        <input className="block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" id="setTheme" type="text" placeholder="Set Theme" value={set["Theme"]} onChange={(e)=>setSet({...set, Theme: e.target.value})}/>
                     </div>
                     <div className="flex flex-wrap justify-center">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="setCode"> Year released </label>
-                        <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setYear" type="text" placeholder="Set Year" value={set["Year released"]} onChange={(e)=>setSet({...set, Year: e.target.value})}/>
+                        <label className="block w-1/6 mr-1 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" htmlFor="setCode"> Year released </label>
+                        <input className="block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" id="setYear" type="text" placeholder="Set Year" value={set["Year released"]} onChange={(e)=>setSet({...set, Year: e.target.value})}/>
                     </div>
                     <div className="flex flex-wrap justify-center">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="setCode"> Price </label>
-                        <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setPrice" type="text" placeholder="Set Price" value={set["Price"]} onChange={(e)=>setSet({...set, Price: e.target.value})}/>
+                        <label className="block w-1/6 mr-1 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" htmlFor="setCode"> Price </label>
+                        <input className="block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" id="setPrice" type="text" placeholder="Set Price" value={set["Price"]} onChange={(e)=>setSet({...set, Price: e.target.value})}/>
                     </div>
                     <div className="flex flex-wrap justify-center">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="setCode"> RRP </label>
-                        <input className="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="setRRP" type="text" placeholder="Set RRP" value={set["RRP"]} onChange={(e)=>setSet({...set, RRP: e.target.value})}/>
+                        <label className="block w-1/6 mr-1 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" htmlFor="setCode"> RRP </label>
+                        <input className="block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white" id="setRRP" type="text" placeholder="Set RRP" value={set["RRP"]} onChange={(e)=>setSet({...set, RRP: e.target.value})}/>
                     </div>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={()=>confirmSet()}>Add Set</button>
                 </div>
--- frontend/src/pages/StoredSets.jsx
+++ frontend/src/pages/StoredSets.jsx
@@ -2,12 +2,17 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useState } from 'react';
+import LegoSetCard from '../components/LegoSetCard';
 
 const StoredSets = () => {
     const [sets, setSets] = useState([])
     useEffect(() => {
         getSets()
     },[])
+    
+    const refreshSets = () =>{
+        console.log("Refreshing")
+    }
 
     const getSets = () => {
         const userCookie= Cookies.get('token')
@@ -34,15 +39,17 @@ const StoredSets = () => {
     return (
         <div  className="pt-8 px-20 w-full">
             <div className="text-center">
-                <h1 className="text-5xl border-b-2 border-gray-300 ">Stored sets</h1>
+                <h1 className="text-5xl  border-gray-300 ">Stored sets</h1>
+                <div className="bg-gray-300 flex flex-row-reverse m-5 rounded mt-0 ml-0 w-full ">
+                <button className="bg-red-500 border-white rounded-lg text-white m-3 pl-8 pr-8" onClick={()=>refreshSets()}>Refresh</button>
+                </div>
                 <div className="flex flex-row flex-wrap w-full ">
                     {sets.map((set) => ( 
-                        console.log(set.additionalData["Name"]),
                         <div key={set._id} className="border-2 border-gray-300 rounded-xl m-2 p-2 w-full text-left">
                             <p className="text-2xl">{set.additionalData["Name"]}</p>
                             <p className="text-lg">{set.additionalData["Year released"]}</p>
                             <p className="text-lg">{set.additionalData["Theme"]}</p>
-                            <p className="text-lg text-right">{set.additionalData["Price"]}</p>
+                            <p className="text-lg text-right ">{set.additionalData["Price"]}</p>
                         </div>
                     ))}
                     </div>
